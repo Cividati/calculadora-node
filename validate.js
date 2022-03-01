5f0d642a6bfac01a5f98eec9271897b052fcaf97
Cividati <rubens.cividati.teles@gmail.com>
2022-03-01 20:44:53 -0300

    feat: num1, num2 and op vars types defined

---
+++ validate.js
@@ -0,0 +1,13 @@
+const requestConstraint = {
+    num1: {
+        type: "number"
+    },
+    num2: {
+        type: "number"
+    },
+    op: {
+        type: "string"
+    }
+}
+
+module.exports = { requestConstraint };
